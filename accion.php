16ffd1f7e447e4f62af7d0355ecc733f301e1414
JhonEdgar4 <jhonedgarsalazar@gmail.com>
2024-05-18 15:34:46 -0500

    borrar

--- accion.php
+++ accion.php
@@ -2,8 +2,7 @@
 #Inicailizacion de la sesion
 session_start();
 
-#Imprimir contenido de una variable
-//var_dump($_POST);
+
 var_dump($_POST);
 
 #Guardamos en variales de sesion el Email y la contrasena
